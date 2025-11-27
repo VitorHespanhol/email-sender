03f6ea36b03bd0c2ec89935d0bfcbd4d97260943
VitorHespanhol <hespanhol-10@hotmail.com>
2025-11-27 11:19:01 -0300

    Initial Commit

---
+++ src/main/java/com/ahsystems/emailservice/core/EmailRequest.java
@@ -0,0 +1,4 @@
+package com.ahsystems.emailservice.core;
+
+public record EmailRequest(String to, String subject, String body) {
+}
---
+++ src/main/java/com/ahsystems/emailservice/infra/ses/AwsSesConfig.java
@@ -0,0 +1,39 @@
+package com.ahsystems.emailservice.infra.ses;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
+
+
+@Configuration
+public class AwsSesConfig {
+    @Value("${aws.region}")
+    private String region;
+
+    @Value("${aws.accessKeyId}")
+    private String accessKeyId;
+
+    @Value("${aws.secretKey}")
+    private String secretKey;
+
+//    @Bean
+//    public AmazonSimpleEmailService amazonSimpleEmailService(){
+//        return AmazonSimpleEmailServiceClientBuilder.standard().build();
+//    }
+
+    @Bean
+    public AmazonSimpleEmailService amazonSimpleEmailService(){
+        return AmazonSimpleEmailServiceClientBuilder.standard()
+                .withRegion(region)
+                .withCredentials(
+                        new AWSStaticCredentialsProvider(
+                                new BasicAWSCredentials(accessKeyId, secretKey)
+                        )
+                )
+                .build();
+    }
+}
